c31e9f027e08fac2c4175bf210bbd0860449f2c5
KarthiObli <156739270+KarthiObli@users.noreply.github.com>
2025-07-20 23:15:40 +0530

    Add files via upload

---
+++ Infosys_Raw_Data.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS "Raw_Layer"."Infosys_Raw_Data" 
+	(
+		"Date" DATE NOT NULL, 
+		"Price" NUMERIC(7, 2) NOT NULL
+	);
---
+++ Wipro_Raw_Data.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS "Raw_Layer"."Wipro_Raw_Data" 
+	(
+		"Date" DATE NOT NULL, 
+		"Price" NUMERIC(7, 2) NOT NULL
+	);
